d5e79c247998cbdeebe6d8624ace83cb4ad62e2b
Mikael <145621264+mikael717@users.noreply.github.com>
2025-02-03 09:54:37 -0300

    Delete src directory

--- src/main/java/br/com/hope/tabelafipe/services/IConvertData.java
+++
@@ -1,9 +0,0 @@
-package br.com.hope.tabelafipe.services;
-
-import java.util.List;
-
-public interface IConvertData {
-    <T> T obtainData (String json, Class<T> tClass);
-
-    <T> List<T> obtainList (String json, Class<T>tClass);
-}
